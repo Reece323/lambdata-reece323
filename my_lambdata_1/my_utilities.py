6f465aa371abdad7e374c9621320e345140b4960
Reece323 <codyreece323@gmail.com>
2020-09-07 05:38:50 -0500

    changes to my_utilities

--- my_lambdata_1/my_utilities.py
+++ my_lambdata_1/my_utilities.py
@@ -2,6 +2,7 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 
+
 class MyDataSplitter():
     ''' 
     This class implements a 3-way data split and outputs summary metrics. 
@@ -9,36 +10,41 @@ class MyDataSplitter():
 
     def __init__(self, df):
         self.df = df
-
-
-
-
-def train_validation_test_split(self, features, target,
-                                    train_size=0.7, val_size=0.1,
-                                    test_size=0.2, random_state=None,
-                                    shuffle=True):
+        
+        
+    def train_validation_test_split(self, features, target, 
+                                train_size = 0.7, val_size = 0.1, 
+                                test_size = 0.2, random_state = None, 
+                                shuffle = True):
         '''
         This function is a utility wrapper around the Scikit-Learn train_test_split that splits arrays or 
         matrices into train, validation, and test subsets.
+
         Args:
             X (Numpy array or DataFrame): This is a dataframe with features.
+
             y (Numpy array or DataFrame): This is a pandas Series with target.
+
             train_size (float or int): Proportion of the dataset to include in the train split (0 to 1).
+
             val_size (float or int): Proportion of the dataset to include in the validation split (0 to 1).
+
             test_size (float or int): Proportion of the dataset to include in the test split (0 to 1).
+
             random_state (int): Controls the shuffling applied to the data before applying the split for reproducibility.
+
             shuffle (bool): Whether or not to shuffle the data before splitting
+
         Returns:
             Train, test, and validation dataframes for features (X) and target (y). 
         '''
+
         X_train_val, X_test, y_train_val, y_test = train_test_split(
-            self.X, self.y, test_size=test_size, random_state=random_state, shuffle=shuffle)
+            self.X, self.y, test_size = test_size, random_state = random_state, shuffle = shuffle)
+
         X_train, X_val, y_train, y_val = train_test_split(
-            X_train_val, y_train_val, test_size=val_size / (train_size + val_size),
-            random_state=random_state, shuffle=shuffle)
+            X_train_val, y_train_val, test_size = val_size / (train_size + val_size), 
+            random_state = random_state, shuffle = shuffle)
+            
         return X_train, X_val, X_test, y_train, y_val, y_test
-
-if __name__ == "__main__":
-
-
-#tbc
+        
