75350771e6cf9700dfd893004381ce697294c2cd
Reece323 <codyreece323@gmail.com>
2020-09-07 17:33:19 -0500

    Changes to utilities

--- my_lambdata_1/my_utilities.py
+++ my_lambdata_1/my_utilities.py
@@ -1,6 +1,6 @@
-
 import pandas as pd
 from sklearn.model_selection import train_test_split
+
 
 class MyDataSplitter():
     ''' 
@@ -9,11 +9,12 @@ class MyDataSplitter():
 
     def __init__(self, df):
         self.df = df
-    
-    def train_validation_test_split(self, features, target,
-                                    train_size=0.7, val_size=0.1,
-                                    test_size=0.2, random_state=None,
-                                    shuffle=True):
+        
+        
+    def train_validation_test_split(self, features, target, 
+                                train_size = 0.7, val_size = 0.1, 
+                                test_size = 0.2, random_state = None, 
+                                shuffle = True):
         '''
         This function is a utility wrapper around the Scikit-Learn train_test_split that splits arrays or 
         matrices into train, validation, and test subsets.
@@ -28,14 +29,12 @@ class MyDataSplitter():
         Returns:
             Train, test, and validation dataframes for features (X) and target (y). 
         '''
+
         X_train_val, X_test, y_train_val, y_test = train_test_split(
-            self.X, self.y, test_size=test_size, random_state=random_state, shuffle=shuffle)
+            self.X, self.y, test_size = test_size, random_state = random_state, shuffle = shuffle)
+
         X_train, X_val, y_train, y_val = train_test_split(
-            X_train_val, y_train_val, test_size=val_size / (train_size + val_size),
-            random_state=random_state, shuffle=shuffle)
+            X_train_val, y_train_val, test_size = val_size / (train_size + val_size), 
+            random_state = random_state, shuffle = shuffle)
+            
         return X_train, X_val, X_test, y_train, y_val, y_test
-
-if __name__ == "__main__":
-
-
-#tbc
