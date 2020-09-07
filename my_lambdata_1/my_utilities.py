70f8c6d7192a7082f3706c8d29008394fd0eb7e0
Reece323 <codyreece323@gmail.com>
2020-09-07 05:38:50 -0500

    changes to my_utilities

--- my_lambdata_1/my_utilities.py
+++ my_lambdata_1/my_utilities.py
@@ -18,14 +18,22 @@ class MyDataSplitter():
         '''
         This function is a utility wrapper around the Scikit-Learn train_test_split that splits arrays or 
         matrices into train, validation, and test subsets.
+
         Args:
             X (Numpy array or DataFrame): This is a dataframe with features.
+
             y (Numpy array or DataFrame): This is a pandas Series with target.
+
             train_size (float or int): Proportion of the dataset to include in the train split (0 to 1).
+
             val_size (float or int): Proportion of the dataset to include in the validation split (0 to 1).
+
             test_size (float or int): Proportion of the dataset to include in the test split (0 to 1).
+
             random_state (int): Controls the shuffling applied to the data before applying the split for reproducibility.
+
             shuffle (bool): Whether or not to shuffle the data before splitting
+
         Returns:
             Train, test, and validation dataframes for features (X) and target (y). 
         '''
